55ab025e7b3809e7f73eb1f501d4a6a6ea2bc8c8
Auf Wiedersehen <aufwiedersehen212@gmail.com>
2014-10-20 18:30:58 +0700

    Introduce you guys to QuayManager.

---
+++ src/com/dean/QuayManager.java
@@ -0,0 +1,45 @@
+package com.dean;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import android.content.Context;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class QuayManager {
+	private static List<Quay> quays;
+
+	public static List<Quay> getQuaysList(Context context) {
+		if (quays == null) {
+			quays = new ArrayList<Quay>(Arrays.asList(getQuays(context)));
+		}
+		return quays;
+	}
+
+	public static Quay[] getQuays(Context context) {
+		Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
+		return (gson).fromJson(loadJSONFromAsset(context), Quay[].class);
+	}
+
+	public static String loadJSONFromAsset(Context context) {
+		String json = null;
+		try {
+			InputStream is = context.getAssets().open("data.json");
+			int size = is.available();
+			byte[] buffer = new byte[size];
+			is.read(buffer);
+			is.close();
+			json = new String(buffer, "UTF-8");
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			return null;
+		}
+		return json;
+	}
+}
--- src/com/dean/TransportationFragment.java
+++ src/com/dean/TransportationFragment.java
@@ -1,9 +1,5 @@
 package com.dean;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import android.os.Bundle;
@@ -13,10 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ExpandableListView;
 
-import com.google.gson.FieldNamingPolicy;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 public class TransportationFragment extends Fragment {
 	private static final String TAG = "TransportationFragment";
 	private ExpandableListView expandableListView;
@@ -25,33 +17,13 @@ public class TransportationFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_transportation, container, false);
 		expandableListView = (ExpandableListView) rootView.findViewById(R.id.listview);
-		Quay[] quays = getQuays();
-		List<Quay> quaysList = new ArrayList<Quay>(Arrays.asList(quays));
-
-		adapter = new ExpandableListAdapter(getActivity(), expandableListView, quaysList);
-
-		expandableListView.setAdapter(adapter);
+		setupList();
 		return rootView;
 	}
-	public Quay[] getQuays() {
-		Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
-		return (gson).fromJson(loadJSONFromAsset(), Quay[].class);
-	}
 
-	public String loadJSONFromAsset() {
-		String json = null;
-		try {
-			InputStream is = getActivity().getAssets().open("data.json");
-			int size = is.available();
-			byte[] buffer = new byte[size];
-			is.read(buffer);
-			is.close();
-			json = new String(buffer, "UTF-8");
-		} catch (IOException ex) {
-			ex.printStackTrace();
-			return null;
-		}
-		return json;
+	private void setupList() {
+		List<Quay> quaysList = QuayManager.getQuaysList(getActivity());
+		adapter = new ExpandableListAdapter(getActivity(), expandableListView, quaysList);
+		expandableListView.setAdapter(adapter);
 	}
-
 }
